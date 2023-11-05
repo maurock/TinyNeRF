5e39c234bb890863009e9d6e1ab6542c505be62e
maurock <maurocomi92@gmail.com>
2023-11-05 11:13:30 +0000

    Add download bash

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz -O tiny_nerf_data.npz
-unzip tiny_nerf_data.npz
-mv tiny_nerf_data/nerf_synthetic/lego data/lego
-mv tiny_nerf_data/nerf_llff_data/fern data/fern
-rm tiny_nerf_data.npz
-rm -rf tiny_nerf_data
+wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip -O nerf_example_data.zip
+unzip nerf_example_data.zip
+mv nerf_example_data/nerf_synthetic/lego data/lego
+mv nerf_example_data/nerf_llff_data/fern data/fern
+rm nerf_example_data.zip
+rm -rf nerf_example_data
